0c8e1fea03a3a04ebefb23ed15e6ab55acc55f8c
FrancescoBerisso <115414198+FrancescoBerisso@users.noreply.github.com>
2022-12-16 21:24:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
-  return 0;
+    char list[10][20];
+    char nomi[20];
+    int p = 0;
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 20; j++){
+            nomi[j] = ' ';
+            list[i][j] = ' ';
+        }
+    }
+    for(int i = 0; i < 10; i++){
+        cin >> list[i];
+    }
+    cin >> nomi;
+    if(comp(list,p,nomi)) cout << p;
+    else cout << "non presente";
+    return 0;
 }
